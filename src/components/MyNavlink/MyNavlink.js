4322468248a8048700272cf70a2bfc2593062bb7
Mark giliadov <markgiliadov@gmail.com>
2022-04-26 19:08:28 +0300

    styling for toolbar and navlinks

--- src/components/MyNavlink/MyNavlink.js
+++ src/components/MyNavlink/MyNavlink.js
@@ -5,32 +5,30 @@ const MyNavlink = (props) => {
   return (
     <div className={classes.nav}>
       <NavLink
-        // style={{
-        //   display: "inline-block",
-        //   backgroundColor: "#BC986A",
-        //   width: "100%",
-        //   height: "auto",
-        //   textAlign: "center",
-        //   // padding: "1em 0em",
-        //   // margin: "20px",
-        //   textDecoration: "none",
-        // }}
         style={({ isActive }) =>
           isActive
             ? {
-                backgroundColor: "#BC986A",
+                backgroundColor: "#DAAD86",
                 width: "100%",
                 height: "auto",
                 textAlign: "center",
-                color: "#fff",
-                background: "#7600dc",
+                color: "#450202",
+                background: "#9b9bf1",
                 textDecoration: "none",
+                fontSize: "25px",
+                marginBottom: "15px",
+                border: "solid #450202",
               }
-            : { color: "#545e6f", background: "#f0f0f0" }
+            : {
+                marginBottom: "2px",
+                fontSize: "18px",
+                color: "whitesmoke",
+                background: "#DAAD86",
+              }
         }
         to={props.directTo}
       >
-        <Button>{props.linkName}</Button>
+        {props.linkName}
       </NavLink>
     </div>
   );
