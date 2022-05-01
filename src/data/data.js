1926139cc60c66254af7c5f8e99374373e78c420
Yarden Dahan <68491359+Jordan-me@users.noreply.github.com>
2022-05-01 17:06:04 +0300

    update fetch- data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,33 @@
 const GET_USER_ENDPOINT =
-  "http://localhost:8085/iob/users/login/2022b.yarden.dahan/admin@google.com";
+"http://localhost:8085/iob/users/login/2022b.yarden.dahan/admin@google.com";
+const GET_INSTANCE_ENDPOINT =
+  "http://localhost:8085/iob/instances?userDomain=2022b.yarden.dahan&userEmail=manager@google.com";
+  export const fetchUser = async () => {
+    const data = await fetch(GET_USER_ENDPOINT, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        // "Access-Control-Allow-Origin": "*",
+        "Accept": "application/json",
+      },
+      
+    }).then((rawData) => rawData.json());
+    return data;
+  };
+  export const fetchInstance = async () => {
+    
+    const response = await fetch(GET_INSTANCE_ENDPOINT, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET, DELETE, HEAD, OPTIONS",
+        "Accept": "application/json",
+      }
+      ,
 
-export const fetchData = async () => {
-  const data = await fetch(GET_USER_ENDPOINT, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      // "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
-  }).then((rawData) => rawData.json());
-  return data;
-};
+    }).then(response => response.json())
+    .catch(error => console.log('Authorization failed: ' + error.message));
+    return response
+  };
